74fbe79615cdc5e78b57261639f653471630c254
vaibhavtyagi3865 <tyagiv646@gmail.com>
2020-04-11 15:19:43 +0530

    Identifiers,Keywords and Scope:(local,global)

---
+++ Identifiers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var z = 5 //For global scope we use var keyword
+var g int //if global variable is not assigned any value ,then we have to mention its type,otherwise we will get error
+func main() {
+	x := 40 // ":=" will not work outside the local scope .
+	fmt.Println("x=", x)
+	x = 5
+	fmt.Println("x again=", x)
+	fmt.Println("z=", z)
+	foo()
+}
+func foo() {
+	fmt.Println("z again=", z)
+	fmt.Println("g=", g)
+}
