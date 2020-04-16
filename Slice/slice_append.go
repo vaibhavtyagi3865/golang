a5ce31e4b38894b53eb460f911808618541b3d56
vaibhavtyagi3865 <tyagiv646@gmail.com>
2020-04-16 19:34:25 +0530

    Slice :Append and Delete

--- Slice/slice_append.go
+++ Slice/slice_append.go
@@ -9,6 +9,6 @@ func main() {
 	fmt.Println("x[]:", x)
 	//2nd way
 	y := []int{2, 4, 6, 8, 9}
-	x = append(x, y...) // '...' is used to take out all values from array x and insert them in slice x
+	x = append(x, y...) // '...' is used to take out all values from slice y and insert them in slice x
 	fmt.Println("x[]:", x)
 }
